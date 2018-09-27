2d21ed97b8af404c5b43a9fdee3ed0767a120e1b
danvandeee <danvandeee@hotmail.com>
2018-09-27 13:12:40 +0200

    map name fix

--- src/main/java/com/leraar/service/FeedbackService.java
+++ src/main/java/com/leraar/service/FeedbackService.java
@@ -1,5 +1,14 @@
 package com.leraar.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.leraar.dao.IFeedbackDAO;
+
 public class FeedbackService {
 
+	@Autowired
+	IFeedbackDAO iFeedbackDAO;
+	
+	
+	
 }
