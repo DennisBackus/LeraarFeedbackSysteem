5341030191f5f894bccd0f2ae73222950435deac
danvandeee <danvandeee@hotmail.com>
2018-10-03 11:56:53 +0200

    leerling en person worden weer toegevoegd

---
+++ src/main/java/com/leeraar/services/IleerlingService.java
@@ -0,0 +1,11 @@
+package com.leeraar.services;
+
+import com.leeraar.models.Feedback;
+import com.leeraar.repositories.IPersonRepository;
+
+public interface IleerlingService extends IPersonRepository{
+	
+
+ 	public void createFeedBack (Feedback feedback);
+
+}
